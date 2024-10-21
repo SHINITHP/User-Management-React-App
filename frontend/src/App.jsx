3d14f677dfa581e30fdd9246e01d5f0031113a8a
shinithp <shinithp78@gmail.com>
2024-10-22 01:12:32 +0530

    LoginRegister design  added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginRegister from "./components/user/LoginRegister/LoginRegister.jsx";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+      <Route path="/login" element={<LoginRegister />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
